7dfec9cc233d91aec7b954e9f4dafb6201807a60
dan pittman <dan@dpitt.me>
2023-12-06 09:51:45 -0800

    dnf normalization and parser fixes

--- src/Referee.hs
+++ src/Referee.hs
@@ -1,17 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Referee where
 
-import Prelude hiding (repeat)
-import Data.Text
+import Prelude hiding (and, not, or, repeat)
+import Data.Text hiding (concatMap, map)
 import Text.Parselet
-
-newtype Arg =
-  Arg Text
-  deriving (Show)
-  
-newtype Statement =
-  Statement Text
-  deriving (Show)
 
 data Spec
   = Var Text
@@ -23,17 +15,30 @@ data Spec
 spec :: Parser Spec
 spec =
   (do
+     one '!'
      one '('
      s <- spec
      one ')'
-     (do
-        whitespace
-        c <- parseConnective
-        whitespace
-        fmap (c s) spec)
-       <|> pure s)
-    <|> (one '!' >> Not <$> spec)
-    <|> parseSpec
+     con Not s <|> pure (Not s))
+    <|> (do
+           one '('
+           s <- spec
+           one ')'
+           con id s <|> pure s)
+    <|> (do
+           v <- var
+           whitespace
+           c <- parseConnective
+           whitespace
+           c v <$> spec)
+    <|> var
+  where
+    con f s = do
+      whitespace
+      c <- parseConnective
+      whitespace
+      fmap (c (f s)) spec
+    var = (one '!' >> Not . Var <$> word) <|> (Var <$> word)
 
 parseConnective :: Parser (Spec -> Spec -> Spec)
 parseConnective = fmap f (phrase "||" <|> phrase "&&")
@@ -44,16 +49,30 @@ parseConnective = fmap f (phrase "||" <|> phrase "&&")
       | t == "&&" = And
       | t == "||" = Or
 
-parseSpec :: Parser Spec
-parseSpec = assocr var cv
+type Dnf = [[Term]]
+
+data Term
+  = T Text
+  | N Text
+  deriving (Show)
+
+dnf :: Spec -> Dnf
+dnf (Or a b) = or (dnf a) (dnf b)
   where
-    var = do
-      whitespace
-      v <-
-        (do
-           one '!'
-           Not . Var <$> word)
-          <|> (Var <$> word)
-      whitespace
-      pure v
-    cv = parseConnective
+    or xs ys = xs ++ ys
+dnf (And a b) = and (dnf a) (dnf b)
+  where
+    and xs ys =
+      let c = [(x, y) | x <- xs, y <- ys]
+       in map (uncurry (++)) c
+dnf (Var v) = [[T v]]
+dnf (Not s) = not (dnf s)
+  where
+    not xs = or' $ concatMap (map negate) xs
+    or' (x:xs) = [x] : or' xs
+    or' [] = []
+    negate (T v) = N v
+    negate (N v) = T v
+
+dnf' :: Text -> Maybe Dnf
+dnf' t = dnf . fst <$> runParser t spec
